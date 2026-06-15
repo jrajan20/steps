de290233346636bf95368df3a5abd3ecf2f8609a
jrajan20 <jai_20j@hotmail.com>
2026-06-15 15:43:46 -0400

    Added open and close state

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ const messages = [
 
 export default function App() {
   const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
+
 
   function handlePrevious(){
     if(step > 1){
@@ -20,11 +22,14 @@ export default function App() {
   }
 
   return (
-    <div className="steps">
+    <div>
+      <button className="close" onClick={() => setIsOpen(!isOpen)}>&times;</button>
+      {
+        isOpen && (<div className="steps">
       <div className="numbers">
-        <div className={`${step >= 1? "active" : ""}`}>1</div>
-         <div className={`${step >=2? "active" : ""}`}>2</div>
-          <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+        <div className={step >= 1? "active" : ""}>1</div>
+         <div className={step >=2? "active" : ""}>2</div>
+          <div className={step >= 3 ? "active" : ""}>3</div>
       </div>
       <p className="message">Step {step} : {messages[step -1]}</p>
       <div className="buttons">  
@@ -33,6 +38,11 @@ export default function App() {
       </div>
        
     </div>
+   
+    )}
+     </div>
+      
+    
   );
 }
 
