f8cb500a3b1a2d30077a7dc44c597881e6f4ef89
jrajan20 <jai_20j@hotmail.com>
2026-06-16 18:52:11 -0400

    minor css change for  duplicate step component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,15 @@ const messages = [
 ]
 
 export default function App() {
+return(
+  <div>
+    <Steps/>
+    <Steps/>
+  </div>
+)
+}
+
+function Steps(){
   const [step, setStep] = useState(1);
   const [isOpen, setIsOpen] = useState(true);
 
@@ -24,7 +33,7 @@ export default function App() {
   }
 
   return (
-    <>
+    <div>
       <button className="close" onClick={() => setIsOpen((is) => !is)}>&times;</button>
       { 
         isOpen && (<div className="steps">
@@ -42,7 +51,7 @@ export default function App() {
     </div>
    
     )}
-     </>
+     </div>
       
     
   );
